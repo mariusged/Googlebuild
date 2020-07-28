6f190ff31c50c584838b6924f9b2b5ac4924e3b9
mariusged <67638954+mariusged@users.noreply.github.com>
2020-07-28 15:20:10 +0200

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log('New connection')
-    res.end('Hello World - mit ci cd')
-})
-const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log('Hello world listening on port', PORT))
